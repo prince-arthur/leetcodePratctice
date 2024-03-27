59e2a33526fcf234730eb64438626d23e24d1f2f
Nitesh Singh <nikhilsingh0504@gmail.com>
2024-03-27 22:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (64.20%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,7 +10,8 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        return recursiveReverseList(head,null);
+        // return recursiveReverseList(head,null);
+        return iterativeReverseList(head);
     }
 
     private ListNode recursiveReverseList(ListNode head, ListNode newHead){
@@ -19,5 +20,16 @@ class Solution {
         ListNode next = head.next;
         head.next = newHead;
         return recursiveReverseList(next, head);
+    }
+
+    private ListNode iterativeReverseList(ListNode head) {
+        ListNode newHead = null;
+        while(null!=head){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
     }
 }
