53458985f20b2f4dcc5f24801d8c2ca6a3180656
Nitesh Singh <nikhilsingh0504@gmail.com>
2024-06-02 22:16:31 +0530

    Time: 710 ms (49.28%) | Memory: 73.6 MB (10.77%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,33 +1,55 @@
 class Solution {
-    boolean isPalindrome(String s,int start,int end){
-		while(start<end){
-			if(s.charAt(start)!=s.charAt(end))return false;
-			start++;
-			end--;
-		}
-		return true;
-	}
-	int getMinCutsUtil(String s,int start,int end,int[][] dp){
-		if(start>=end)return 0;
-		if(isPalindrome(s,start,end)==true)return dp[start][end]=0;
-		if(dp[start][end]!=-1)return dp[start][end];
-		int mini=Integer.MAX_VALUE;
-		for(int i=start;i<end;i++){
-			int res=Integer.MAX_VALUE;
-			if(isPalindrome(s,start,i)){
-				res=1+getMinCutsUtil(s,i+1,end,dp);
-			}
-			mini=Math.min(mini,res);
-		}
-		return dp[start][end]=mini;
-	}
     public int minCut(String s) {
-        int[][] dp=new int[s.length()][s.length()];
-		for(int i=0;i<dp.length;i++){
-			for(int j=0;j<dp[0].length;j++){
-				dp[i][j]=-1;
-			}
-		}
-		return getMinCutsUtil(s,0,s.length()-1,dp);
+        char[] S = s.toCharArray();
+        int i = 0, j = S.length - 1;
+        int[][] t = new int[S.length+1][S.length+1];
+        for(int row[]:t)
+            Arrays.fill(row,-1);
+        return solve(S,i,j,t);
+    }
+    public int solve(char[] S, int i, int j, int[][] t){
+        if(i>=j)
+            return 0;
+        if(t[i][j]!=-1)
+            return t[i][j];
+        if(isPalindrome(S,i,j) == true)
+            return 0;
+        int min = Integer.MAX_VALUE, left = 0, right = 0;
+        for(int k=i; k<j; k++){
+            // if(t[i][k]!=-1)
+            //     left = t[i][k];
+            // else{
+            //     left = solve(S,i,k,t);
+            //     t[i][k] = left;
+            // }
+
+            // if(t[k+1][j]!=-1)
+            //     right = t[k+1][j];
+            // else{
+            //     right = solve(S,k+1,j,t);
+            //     t[k+1][j] = right;
+            // }
+            
+            // int temp = left + right + 1;
+           
+            // if(temp<min)
+            //     min = temp;
+             if(isPalindrome(S,i,k)){
+                int temp=1+solve(S,k+1,j,t);
+                min=Math.min(min,temp);
+            }
+        }
+        return t[i][j] = min;
+    }
+    public boolean isPalindrome(char[] S, int i, int j){
+        if(i>=j)
+            return true;
+        while(i<j){
+            if(S[i]!=S[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
