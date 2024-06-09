09317b5162d90fbe47781d623a437b10e3de8165
Nitesh Singh <nikhilsingh0504@gmail.com>
2024-06-09 20:27:49 +0530

    Time: 1 ms (100.00%) | Memory: 60.7 MB (50.00%) - LeetSync

---
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/find-the-first-player-to-win-k-games-in-a-row.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findWinningPlayer(int[] skills, int k) {
+        int topPlayer = skills[0], curr = 0, ans = 0;
+        for(int i=1; i<skills.length; i++){
+            if(topPlayer < skills[i]){
+                topPlayer = skills[i];
+                curr = 0;
+                ans = i;
+            }
+            if(++curr == k) break;
+        }
+        return ans;
+    }
+}
