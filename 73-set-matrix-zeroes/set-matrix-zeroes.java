39b7eaf9ec9db9af38f1dc5822abaf7d391467a3
Nitesh Singh <nikhilsingh0504@gmail.com>
2024-07-14 19:50:24 +0530

    Time: 1 ms (81.31%) | Memory: 45.8 MB (11.34%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -5,35 +5,24 @@ class Solution {
 
     private void setZeroes(int[][] matrix, int n, int m) {
         boolean firstRow = false;
-        
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == 0){
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) 
+                if(0 == matrix[i][j]){
                     matrix[0][j] = 0;
-                    if(i == 0) {
-                        firstRow = true;
-                    } else {
-                        matrix[i][0] = 0;
-                    }
+                    if(0 == i) firstRow = true;
+                    else matrix[i][0] = 0;
                 }
-            }
-        }
-
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i < n; i++){
             for(int j = 1; j < m; j++) {
-                if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                if(0 == matrix[i][0]|| 0 == matrix[0][j])
                     matrix[i][j] = 0;
             }
         }
-        if(matrix[0][0] == 0) {
-            for(int i = 0; i < n; i++){
+        if(0 == matrix[0][0])
+            for(int i = 0; i < n; i++)
                 matrix[i][0] = 0;
-            }
-        }
-        if(firstRow) {
-            for(int j = 0; j < m; j++) {
+        if(firstRow)
+            for(int j = 0; j < m; j++)
                 matrix[0][j] = 0;
-            }
-        }
     }
 }
