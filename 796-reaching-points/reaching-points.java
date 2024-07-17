dc0c88cf390f440c7e1a6f16f35bdaa676ded7aa
Nitesh Singh <nikhilsingh0504@gmail.com>
2024-07-17 20:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (9.47%) - LeetSync

---
+++ 796-reaching-points/reaching-points.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while(sx < tx && sy < ty) {
+            if(tx < ty) ty%=tx;
+            else tx%=ty;
+        }
+        if(sx==tx && sy <= ty && (ty - sy) % sx==0) return true;
+        return sy == ty && sx <= tx && (tx - sx) % sy == 0;
+    }
+}
