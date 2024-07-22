5e8b3188dd71131c90aa6637b53fcabd83bbdd8e
Nitesh Singh <nikhilsingh0504@gmail.com>
2024-07-22 23:32:43 +0530

    Time: 31 ms (60.08%) | Memory: 51.3 MB (63.77%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,40 +1,33 @@
 class Solution {
      //2 pointers
     public List<List<Integer>> threeSum(int[] nums) {
-        int numsLength = nums.length;
-		Arrays.sort(nums);										//[-4,-1,-1,0,1,2]
-		List<List<Integer>> list = new LinkedList<List<Integer>>();
-		for(int i=0; i<numsLength-2; i++) {
-			if(i==0 || (i>0 && nums[i] != nums[i-1])) {
-				int target = 0-nums[i];
-				int left = i + 1;
-				int right = numsLength-1;
-				
-				while(left<right) {
-					if(nums[left] + nums[right] == target) {
-						List<Integer> currSoln = new ArrayList<>();
-						currSoln.add(nums[i]);
-						currSoln.add(nums[left]);
-						currSoln.add(nums[right]);
-						list.add(currSoln);
-						
-						while(left < right && nums[left] == nums[left + 1]) {
-							left++;
-						}
-						
-						while(left < right && nums[right] == nums[right-1]) {
-							right--;
-						}
-						left++;
-						right--;
-					} else if(nums[left] + nums[right] > target) {
-						right--;
-					} else {
-						left++;
-					}
-				}
-			}
-		}
-		return list;
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < n - 2; i++) {
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            // int target=0-nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    List<Integer> tempAns = Arrays.asList(nums[i], nums[left], nums[right]);
+                    ans.add(tempAns);
+                    // left++;
+                    // right--;
+
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {  
+                    left++;
+                } else { 
+                    right --;   
+                }
+            }
+        }
+        return ans;
 	}
 }
