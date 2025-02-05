a8ed0ecfc38f391c700c2d0b104e44b373bd0d0b
Nitesh Singh <nikhilsingh0504@gmail.com>
2025-02-05 21:12:19 +0530

    Time: 13 ms (91.57%) | Memory: 44 MB (65.07%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,42 +1,29 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length; // size of the array
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // sort the given array:
+        List<List<Integer>> res = new ArrayList<>();
+        int n =  nums.length;
         Arrays.sort(nums);
-
-        // calculating the quadruplets:
-        for (int i = 0; i < n; i++) {
-            // avoid the duplicates while moving i:
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n; j++) {
-                // avoid the duplicates while moving j:
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                // 2 pointers:
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-                    long sum = nums[i]; sum += nums[j]; sum += nums[k]; sum += nums[l];
-                    if (sum == target) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add(nums[l]);
-                        ans.add(temp);
-                        k++;
-                        l--;
-
-                        // skip the duplicates:
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
-                    } else if (sum < target) k++;
-                    else l--;
+        for(int i = 0; i < n - 3; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j < n - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j-1]) continue;
+                long newTarget = (long) target - (long) nums[i] - (long) nums[j]; 
+                int low = j+1, high = n-1;
+                while(low < high){
+                    if(nums[low] + nums[high] == newTarget){
+                        res.add(Arrays.asList(nums[low] , nums[high] , nums[i] , nums[j]));
+                        while(low<high && nums[low] == nums[low+1]) low++;
+                        while(low<high && nums[high] == nums[low-1]) high--;
+                        low++;
+                        high--;
+                    } else if(nums[low] + nums[high] < newTarget){
+                        low++;
+                    } else {
+                        high--;
+                    }
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
