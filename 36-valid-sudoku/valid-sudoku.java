d90de1c29eb74c57b8d6894f274f1576089759b6
Nitesh Singh <nikhilsingh0504@gmail.com>
2025-03-31 18:00:31 +0530

    Time: 2 ms (77.65%) | Memory: 44.1 MB (97.78%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,13 +1,35 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set set = new HashSet<>();
-        for(int row = 0; row < 9; row++){
-            for(int col = 0; col < 9; col++) {
-                char number = board[row][col];
-                if('.'!=number ){
-                    if(!set.add(number + "in row " + row) || !set.add(number + "in col " + col) || !set.add(number + " in block " + row/3 + "-" + col/3))
-                        return false;
+        return isValidSudoku(board, board.length);
+    }
+
+    private boolean isValidSudoku(char[][] board, int N){
+        Set<Character>[] rows = new HashSet[N];
+        Set<Character>[] cols = new HashSet[N];
+        Set<Character>[] boxs = new HashSet[N];
+
+        for(int i = 0; i < N; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxs[i] = new HashSet<>();
+        }
+
+        for(int r = 0; r < N; r++) {
+            for(int c = 0; c < N; c++) {
+                char val = board[r][c];
+
+                if(val == '.') {
+                    continue;
                 }
+
+                if(rows[r].contains(val)) return false;
+                rows[r].add(val);
+                if(cols[c].contains(val)) return false;
+                cols[c].add(val);
+                
+                int idx = r/3*3 + c/3;
+                if(boxs[idx].contains(val)) return false;
+                boxs[idx].add(val);
             }
         }
         return true;
