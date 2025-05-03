cae83879bdcac4340d3bf375cf0fae42f88c70cd
Nitesh Singh <nikhilsingh0504@gmail.com>
2025-05-03 10:21:35 +0530

    Time: 32 ms (44.88%) | Memory: 51.5 MB (59.95%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,30 @@
 class Solution {
-     //2 pointers
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < n - 2; i++) {
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            // int target=0-nums[i];
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1;
             int right = n - 1;
             while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
-                    List<Integer> tempAns = Arrays.asList(nums[i], nums[left], nums[right]);
-                    ans.add(tempAns);
-                    // left++;
-                    // right--;
+                int target = nums[left] + nums[right] + nums[i];
+                if(target == 0) {
+                    List<Integer> tempAns = Arrays.asList(nums[left], nums[right], nums[i]);
+                    list.add(tempAns);
 
                     while(left < right && nums[left] == nums[left + 1]) left++;
                     while(left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
-                } else if (sum < 0) {  
+                } else if(target < 0) {
                     left++;
-                } else { 
-                    right --;   
+                } else {
+                    right--;
                 }
             }
         }
-        return ans;
+        return list;
 	}
+    
 }
