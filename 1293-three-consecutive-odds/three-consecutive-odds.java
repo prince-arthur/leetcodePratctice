8ab4c3b2c36d0774e436f39cd96e1bec1d09fe52
Nitesh Singh <nikhilsingh0504@gmail.com>
2025-05-11 20:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (70.92%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i = 1; i < arr.length - 1; i++) {
+            if(isOdd(arr[i-1]) && isOdd(arr[i]) && isOdd(arr[i+1])) return true;
+        }
+        return false;
+    }
+
+    private boolean isOdd(int n){
+        return n%2==1;
+    }
+}
